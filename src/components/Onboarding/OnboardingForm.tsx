c9b3cf059bd2a9e5155c3ce05d8b27c9fabd32ed
Paul L. <13600558+dev-paulL@users.noreply.github.com>
2024-10-25 18:35:37 +0200

    feature(onboarding): added preventdefault on enter to prevent skipping steps. the entire thing needs to be optimised. separated success component

--- src/components/Onboarding/OnboardingForm.tsx
+++ src/components/Onboarding/OnboardingForm.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import db from "../../db";
 import { FavColor, Friend, Gender } from "../../types/UserInfo";
-import InputRadioItem from "../reusable/InputRadioItem";
+
 import Step1 from "./Step1";
 import Step2 from "./Step2";
 import Step3 from "./Step3";
 import Step4 from "./Step4";
+import Success from "./Success";
 
 export interface OnboardingFormData {
   name: string;
@@ -23,7 +24,6 @@ export const friends: Friend[] = ["dog", "cat", "rodent"];
 
 const OnboardingForm: React.FC<OnboardingFormProps> = ({
   setOnboardingCompleted,
-  onboardingCompleted,
 }) => {
   const [onboardingFormData, setOnboardingFormData] =
     useState<OnboardingFormData>({
@@ -32,43 +32,113 @@ const OnboardingForm: React.FC<OnboardingFormProps> = ({
       favColor: undefined,
       friend: undefined,
     });
+  const maxFormSteps = 5;
+  const [onboardingFormStep, setOnboardingFormStep] = useState(1);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const handleNextStep = () => {
+    if (onboardingFormStep === 1 && !onboardingFormData.name) {
+      setErrorMessage("Please enter your name");
+      return;
+    }
+    if (onboardingFormStep === 2 && !onboardingFormData.gender) {
+      setErrorMessage("Please select your gender");
+      return;
+    }
+    if (onboardingFormStep == 3 && !onboardingFormData.favColor) {
+      setErrorMessage("Please select your favorite color");
+      return;
+    }
+    if (onboardingFormStep === 4 && !onboardingFormData.friend) {
+      setErrorMessage("Please select your friend");
+      return;
+    } else {
+      setErrorMessage(null);
+      if (onboardingFormStep < maxFormSteps)
+        setOnboardingFormStep(onboardingFormStep + 1);
+    }
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLFormElement>) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      handleNextStep(); // Manually move to the next step instead
+    }
+  };
   const handleOnboardingSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    await db.put({ ...onboardingFormData, _id: "onboarding" });
-    setOnboardingCompleted(true);
+    try {
+      await db.put({ ...onboardingFormData, _id: "onboarding" });
+      setOnboardingFormStep(maxFormSteps);
+    } catch (err) {
+      console.error("error saving onboarding: ", err);
+    }
   };
   return (
     <>
-      {onboardingCompleted ? (
-        <div>
-          <h2>Congratulations! You've completed the onboarding process.</h2>
-          <button>Start your journey</button>
-        </div>
-      ) : (
-        <form onSubmit={handleOnboardingSubmit}>
+      <form onSubmit={handleOnboardingSubmit} onKeyDown={handleKeyDown}>
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+
+        {onboardingFormStep === 1 && (
           <Step1
             onboardingFormData={onboardingFormData}
             setOnboardingFormData={setOnboardingFormData}
           />
+        )}
 
+        {onboardingFormStep === 2 && (
           <Step2
             onboardingFormData={onboardingFormData}
             setOnboardingFormData={setOnboardingFormData}
           />
+        )}
 
+        {onboardingFormStep === 3 && (
           <Step3
             onboardingFormData={onboardingFormData}
             setOnboardingFormData={setOnboardingFormData}
           />
+        )}
 
+        {onboardingFormStep === 4 && (
           <Step4
             onboardingFormData={onboardingFormData}
             setOnboardingFormData={setOnboardingFormData}
           />
+        )}
 
-          <button type="submit">Submit</button>
-        </form>
-      )}
+        {onboardingFormStep === maxFormSteps && (
+          <Success setOnboardingCompleted={setOnboardingCompleted} />
+        )}
+        <div
+          className={`flex gap-4 min-w-full ${onboardingFormStep === 1 ? "justify-end" : "justify-between"}`}
+        >
+          {onboardingFormStep > 1 && (
+            <button
+              className="border border-black p-2 rounded-md"
+              type="button"
+              onClick={() =>
+                onboardingFormStep > 1 &&
+                setOnboardingFormStep(onboardingFormStep - 1)
+              }
+            >
+              Prev
+            </button>
+          )}
+          {onboardingFormStep < maxFormSteps - 1 && (
+            <button
+              onClick={handleNextStep}
+              type="button"
+              className="bg-black text-white rounded-md p-2"
+            >
+              Next
+            </button>
+          )}
+          {onboardingFormStep === maxFormSteps - 1 && (
+            <button type="submit">Save informations</button>
+          )}
+        </div>
+      </form>
     </>
   );
 };
