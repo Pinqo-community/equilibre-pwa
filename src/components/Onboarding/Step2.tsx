c5f72476aced37f2c5cbb7d74009cc1ad9f608ca
Paul L. <13600558+dev-paulL@users.noreply.github.com>
2024-10-25 17:50:46 +0200

    feature(onboarding): separated the different form steps into their own component

--- src/components/Onboarding/Step2.tsx
+++ src/components/Onboarding/Step2.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { Gender } from "../../types/UserInfo";
+import InputRadioItem from "../reusable/InputRadioItem";
+import { genders } from "./OnboardingForm";
+import { OnboardingFormData } from "./OnboardingForm";
 
-const Step2: React.FC = () => {
-  return <div>Step2</div>;
+interface Step2Props {
+  onboardingFormData: OnboardingFormData;
+  setOnboardingFormData: React.Dispatch<
+    React.SetStateAction<OnboardingFormData>
+  >;
+}
+const Step2: React.FC<Step2Props> = ({
+  onboardingFormData,
+  setOnboardingFormData,
+}) => {
+  return (
+    <fieldset>
+      <legend>
+        <h2>
+          Nice to meet you {onboardingFormData.name}! How do you identify?
+        </h2>
+      </legend>
+      {genders.map((gender) => (
+        <InputRadioItem
+          key={gender}
+          onChange={(e) =>
+            setOnboardingFormData({
+              ...onboardingFormData,
+              gender: e.target.value as Gender,
+            })
+          }
+          item={gender}
+          state={onboardingFormData.gender}
+          name="gender"
+        />
+      ))}
+    </fieldset>
+  );
 };
 
 export default Step2;
--- src/components/Onboarding/Step3.tsx
+++ src/components/Onboarding/Step3.tsx
@@ -1,7 +1,38 @@
 import React from "react";
-
-const Step3: React.FC = () => {
-  return <div>Step3</div>;
+import { favColors, OnboardingFormData } from "./OnboardingForm";
+import InputRadioItem from "../reusable/InputRadioItem";
+import { FavColor } from "../../types/UserInfo";
+interface Step3Props {
+  onboardingFormData: OnboardingFormData;
+  setOnboardingFormData: React.Dispatch<
+    React.SetStateAction<OnboardingFormData>
+  >;
+}
+const Step3: React.FC<Step3Props> = ({
+  onboardingFormData,
+  setOnboardingFormData,
+}) => {
+  return (
+    <fieldset>
+      <legend>
+        <h2>Thank you! Which color do you prefer?</h2>
+      </legend>
+      {favColors.map((color) => (
+        <InputRadioItem
+          key={color}
+          onChange={(e) =>
+            setOnboardingFormData({
+              ...onboardingFormData,
+              favColor: e.target.value as FavColor,
+            })
+          }
+          item={color}
+          state={onboardingFormData.favColor}
+          name="favColor"
+        />
+      ))}
+    </fieldset>
+  );
 };
 
 export default Step3;
