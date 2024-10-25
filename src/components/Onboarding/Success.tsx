dc3ec79f8e13f004d099a27a2abb53c080344666
Paul L. <13600558+dev-paulL@users.noreply.github.com>
2024-10-25 18:35:37 +0200

    feature(onboarding): added preventdefault on enter to prevent skipping steps. the entire thing needs to be optimised. separated success component

--- src/components/Onboarding/Success.tsx
+++ src/components/Onboarding/Success.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const Success: React.FC = () => {
-  return <div>Success</div>;
+interface SuccessProps {
+  setOnboardingCompleted: (completed: boolean) => void;
+}
+const Success: React.FC<SuccessProps> = ({ setOnboardingCompleted }) => {
+  const handleStartJourney = () => {
+    setOnboardingCompleted(true);
+  };
+  return (
+    <div>
+      <h2>Congratulations! You've completed the onboarding process.</h2>
+      <button
+        onClick={handleStartJourney}
+        className="flex-1 p-4 bg-black text-white font-bold"
+      >
+        Start your journey
+      </button>
+    </div>
+  );
 };
 
 export default Success;
