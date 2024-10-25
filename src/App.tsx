0bd22f7b315c4a0615a9ff65686a0033fb0e81dc
Garina <garina.leon@gmail.com>
2024-10-25 10:32:56 +0200

    feat(crypto): add some script on app tsx to test encryption on app start

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,39 @@
 import PWABadge from "./pwa/PWABadge.tsx";
 import MoodForm from "./components/MoodForm/MoodForm.tsx";
 
+import {
+  checkIfDEKExists,
+  decryptUserData,
+  encryptUserData,
+  initializeEncryption,
+} from "./common/utils/encryption/encryptionServices.ts";
+
+async function test() {
+  try {
+    //alert("test");
+    if (!(await checkIfDEKExists())) {
+      const password =
+        prompt("Entrez votre mot de passe pour sécuriser vos données :") ||
+        "test";
+
+      console.log("password", password);
+
+      await initializeEncryption(password);
+
+      const userData = "données sensibles";
+      const encrypted = await encryptUserData(userData);
+      console.log("Données chiffrées:", encrypted);
+
+      const decrypted = await decryptUserData(encrypted);
+      console.log("Données déchiffrées:", decrypted);
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+await test();
+
 function App() {
   return (
     <>
