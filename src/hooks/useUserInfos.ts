1efd0999cdf895911de802d8e82f409db7f8c989
Gwenaël Magnenat <g.magnenat@gmail.com>
2024-11-20 12:45:07 +0100

    feat: add useUserInfo hook to fetch user informations

--- src/hooks/useUserInfos.ts
+++ src/hooks/useUserInfos.ts
@@ -1,2 +1,29 @@
-// User infos collected during the onboarding
-// Could create a second hook related to user preferences
+import { useState, useEffect, useCallback } from "react";
+import usePouchDb from "./usePouchDb";
+import { UserInfo } from "../types/UserInfo";
+
+/**
+ * Hook to fetch user information
+ * @returns userInfo and fetchUserInfo
+ */
+const useUserInfo = () => {
+  const { fetchDocs } = usePouchDb<UserInfo>();
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+
+  // Fetch user information
+  const fetchUserInfo = useCallback(async () => {
+    const result = await fetchDocs();
+    if (result.length > 0) {
+      setUserInfo(result[0]);
+    }
+  }, [fetchDocs]);
+
+  // Fetch user information on component mount
+  useEffect(() => {
+    fetchUserInfo();
+  }, [fetchUserInfo]);
+
+  return { userInfo, fetchUserInfo };
+};
+
+export default useUserInfo;
